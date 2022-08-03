f7d91fc35fcbba46cba74a05770506f9828b762d
JonyNhoque <jgabriel.nhoque@gmail.com>
2022-08-03 10:49:29 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Banana e Suco de maçã</h2>
+    </div>
+  );
+}
+
+export default App;
